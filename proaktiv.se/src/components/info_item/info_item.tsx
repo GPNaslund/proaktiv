6a8b5f3996154ed1c4886de9ecfc7778adb9bd7d
GustavPhilipNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-07-14 09:53:19 +0200

    Fixes

--- proaktiv.se/src/components/info_item/info_item.tsx
+++ proaktiv.se/src/components/info_item/info_item.tsx
@@ -19,7 +19,7 @@ const InfoItem: React.FC<InfoItemProps> = ({ infoHeader, infoBody, infoImageSrc,
     }
     return (
         <div className="relative h-screen w-screen tablet:h-full tablet:w-full tablet:rounded tablet:overflow-hidden">
-            <div className="relative z-10 px-10 pt-24 tablet:pt-12 tablet:text-center tablet:grid tablet:grid-cols-2">
+            <div className="relative z-10 px-10 pt-28 tablet:pt-12 tablet:text-center tablet:grid tablet:grid-cols-2">
                 <div>
                     <h1 className="text-white text-2xl pb-2">{infoHeader}</h1>
                     <p className="text-white text-xl pr-5 whitespace-pre-line tablet:px-12">
