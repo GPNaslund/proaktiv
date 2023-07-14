b019e4e4794b9b3272af127cc6b986e279bf23dc
GustavPhilipNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-07-14 18:33:45 +0200

    fixes

--- proaktiv.se/src/components/navbar/navbar.tsx
+++ proaktiv.se/src/components/navbar/navbar.tsx
@@ -1,12 +1,26 @@
 'use client'
 
 import Image from "next/legacy/image"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavBarLinks from './navbar_links';
 import './hamburgermenu.css'
 
 export default function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [showNavbar, setShowNavbar] = useState(true);
+
+    useEffect(() => {
+        let prevScrollPos = window.pageYOffset;
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const scrollingDown = currentScrollPos > prevScrollPos;
+            setShowNavbar(!scrollingDown);
+            prevScrollPos = currentScrollPos;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     const handleMenuClick = () => {
         setIsOpen(!isOpen);
@@ -15,7 +29,7 @@ export default function NavBar() {
     const mobileMenu:string = "lg:hidden fixed top-0 left-0 w-full h-2/5 bg-white flex flex-col items-center justify-center transform transition-transform duration-300 ease-in-out z-20";
 
     return (
-        <nav className="fixed top-0 left-0 right-0 grid grid-cols-4 w-full z-20 p-5">
+        <nav className={`fixed top-0 left-0 right-0 grid grid-cols-4 w-full z-20 p-5 ${showNavbar ? '' : 'fade-out'}`}>
             <div className="col-span-1 flex items-center justify-start z-30">
                 <Image
                     src="/Mini-bones-white.svg"
