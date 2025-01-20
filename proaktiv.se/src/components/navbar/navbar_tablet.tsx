f963a101c01a39153d9d8f047c229800a8252494
GPNaslund <gpnaslund@gmail.com>
2025-01-20 10:24:07 +0100

    Disables booking

--- proaktiv.se/src/components/navbar/navbar_tablet.tsx
+++ proaktiv.se/src/components/navbar/navbar_tablet.tsx
@@ -1,61 +1,68 @@
 import FacebookLink from "@/util/facebook_link";
 import InstagramLink from "@/util/instagram_link";
-import Image from "next/legacy/image"
-
+import Image from "next/legacy/image";
 
 export default function NavBarTablet() {
-    return (
-        <nav className="fixed top-0 left-0 right-0 grid tablet-lg:grid-cols-4 grid-cols-5 w-full z-20 p-5 bg-white" id="main-grid">
-            <div className="col-span-1 flex items-center justify-start z-20">
-                <Image
-                    src="/Mini-bones.svg"
-                    alt="Proaktiv logo"
-                    className=""
-                    width={40}
-                    height={40}
-                />
-            </div>
-            <div className="col-span-2 flex flex-col items-center text-xs justify-self-end tablet-lg:justify-self-center">
-                <a href="mailto:info@proaktividrottscenter.se">info@proaktividrottscenter.se</a>
-                <a href="tel:0812137337">08-12137337</a>
-                <p>Lidingövägen 1, 11433 Stockholm</p>
-            </div>
-            <div className="col-span-2 tablet-lg:col-span-1 justify-self-end grid grid-cols-2">
-                <div className="">
-                    <a href={InstagramLink('proaktividrottscenter')} target="_blank">
-                        <Image
-                            src="/instagram.svg"
-                            alt="instagram icon"
-                            width={35}
-                            height={35}
-                        />
-                    </a>
-                    <a href={FacebookLink('proaktividrottscenter')} target="_blank">
-                        <Image
-                            src="/facebook.svg"
-                            alt="facebook icon"
-                            width={35}
-                            height={35}
-                        />
-                    </a>
-                    <a href="https://goo.gl/maps/BVbHc4pMbpoWM4eq6" target="_blank">
-                        <Image
-                            src="/googlemaps.svg"
-                            alt="google maps icon"
-                            width={35}
-                            height={35}
-                        />
-                    </a>
-                </div>
-                <div className="">
-                <a href="https://system.easypractice.net/book/proaktiv-idrottscenter" target="_blank">
-                    <button className= " text-black text-sm font-bold py-2 px-4 rounded-lg border-black border">
-                        Boka tid
-                    </button>
-                </a>
-            </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav
+      className="fixed top-0 left-0 right-0 grid tablet-lg:grid-cols-4 grid-cols-5 w-full z-20 p-5 bg-white"
+      id="main-grid"
+    >
+      <div className="col-span-1 flex items-center justify-start z-20">
+        <Image
+          src="/Mini-bones.svg"
+          alt="Proaktiv logo"
+          className=""
+          width={40}
+          height={40}
+        />
+      </div>
+      <div className="col-span-2 flex flex-col items-center text-xs justify-self-end tablet-lg:justify-self-center">
+        <a href="mailto:info@proaktividrottscenter.se">
+          info@proaktividrottscenter.se
+        </a>
+        <a href="tel:0812137337">08-12137337</a>
+        <p>Lidingövägen 1, 11433 Stockholm</p>
+      </div>
+      <div className="col-span-2 tablet-lg:col-span-1 justify-self-end grid grid-cols-2">
+        <div className="">
+          <a href={InstagramLink("proaktividrottscenter")} target="_blank">
+            <Image
+              src="/instagram.svg"
+              alt="instagram icon"
+              width={35}
+              height={35}
+            />
+          </a>
+          <a href={FacebookLink("proaktividrottscenter")} target="_blank">
+            <Image
+              src="/facebook.svg"
+              alt="facebook icon"
+              width={35}
+              height={35}
+            />
+          </a>
+          <a href="https://goo.gl/maps/BVbHc4pMbpoWM4eq6" target="_blank">
+            <Image
+              src="/googlemaps.svg"
+              alt="google maps icon"
+              width={35}
+              height={35}
+            />
+          </a>
+        </div>
+        <div className="">
+          <a
+            href="https://system.easypractice.net/book/proaktiv-idrottscenter"
+            target="_blank"
+            className="pointer-events-none"
+          >
+            <button className="text-black text-sm font-bold py-2 px-4 rounded-lg border-black border opacity-20">
+              Boka tid
+            </button>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
-
--- proaktiv.se/src/components/service_item_swiper/service_item_swiper.tsx
+++ proaktiv.se/src/components/service_item_swiper/service_item_swiper.tsx
@@ -1,128 +1,149 @@
-'use client'
+"use client";
 
-import React, { useState, useRef, useEffect } from 'react';
-import './serviceitemswiper.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./serviceitemswiper.css";
 
 type ServiceSwiperProps = {
-    items: React.ReactElement[];
+  items: React.ReactElement[];
 };
 
 const ServiceItemSwiper: React.FC<ServiceSwiperProps> = ({ items }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const containerRef = useRef<HTMLDivElement>(null);
-    const dragData = useRef<{ startX: number; currentX: number } | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const dragData = useRef<{ startX: number; currentX: number } | null>(null);
 
-    useEffect(() => {
-        const handleLinkClick = (event: CustomEvent<{ index: number}>) => {
-            setCurrentIndex(event.detail.index)
-        };
-
-        window.addEventListener('swiperLinkClick', handleLinkClick as EventListener);
-        
-        return () => {
-            window.removeEventListener('swiperLinkClick', handleLinkClick as EventListener);
-        }
-    }, [items]);
-
-    const handleNext = () => {
-        if (currentIndex < items.length - 1) {
-            setCurrentIndex((prevIndex) => prevIndex + 1);
-        }
+  useEffect(() => {
+    const handleLinkClick = (event: CustomEvent<{ index: number }>) => {
+      setCurrentIndex(event.detail.index);
     };
 
-    const handlePrev = () => {
-        if (currentIndex > 0) {
-            setCurrentIndex((prevIndex) => prevIndex - 1);
-        }
-    };
-
-    const handleDragStart = (
-        event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
-      ) => {
-        const startX = 'touches' in event ? event.touches[0].clientX : event.clientX;
-        dragData.current = { startX, currentX: startX };
-      };
-    
-      const handleDragMove = (
-        event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
-      ) => {
-        if (!dragData.current) return;
-        const currentX = 'touches' in event ? event.touches[0].clientX : event.clientX;
-        const { startX } = dragData.current;
-        const diffX = currentX - startX;
-        const threshold = containerRef.current!.offsetWidth / 4;
-    
-        if (diffX > threshold) {
-          handlePrev();
-          dragData.current = null;
-        } else if (diffX < -threshold) {
-          handleNext();
-          dragData.current = null;
-        } else {
-          dragData.current.currentX = currentX;
-        }
-      };
-    
-      const handleDragEnd = () => {
-        dragData.current = null;
-      };
-
-      const swipeToIndex = (index: number) => {
-        if (index >= 0 && index < items.length) {
-            setCurrentIndex(index);
-        }
-      }
-
-    return (
-        <div 
-            className="swiper-container"
-            ref={containerRef}
-            onMouseDown={handleDragStart}
-            onMouseMove={handleDragMove}
-            onMouseUp={handleDragEnd}
-            onTouchStart={handleDragStart}
-            onTouchMove={handleDragMove}
-            onTouchEnd={handleDragEnd}
-        >
-            <div className={`swiper-navigation ${currentIndex > 0 ? 'show-prev' : ''} ${currentIndex < items.length - 1 ? 'show-next' : ''}`}>
-                <button className="swiper-button-prev" onClick={handlePrev}>
-                    &lt;
-                </button>
-                <button className="swiper-button-next" onClick={handleNext}>
-                    &gt;
-                </button>
-            </div>
-            <div className="swiper-wrapper" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-                {items.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`swiper-slide ${index === currentIndex ? 'active' : ''}`}
-                    >
-                        {item}
-                    </div>
-                ))}
-            </div>
-            <div className="pagination-container">
-                <div className="pagination-dots">
-                    {items.map((_, index) => (
-                        <div
-                            key={index}
-                            className={`pagination-dot ${index === currentIndex ? 'active' : ''}`}
-                            onClick={() => {setCurrentIndex(index)}}
-                        />
-                    ))}
-                </div>
-            </div>
-            <div className="book-now-button">
-                <a href="https://system.easypractice.net/book/proaktiv-idrottscenter" target="_blank">
-                    <button className= " text-white font-bold py-2 px-4 rounded-lg border-white border-2">
-                        Boka tid
-                    </button>
-                </a>
-            </div>
-            <p className="contact-info text-xs tracking-widest">Lidingövägen 1, 114 33 Stockholm</p>
-        </div>
+    window.addEventListener(
+      "swiperLinkClick",
+      handleLinkClick as EventListener,
     );
+
+    return () => {
+      window.removeEventListener(
+        "swiperLinkClick",
+        handleLinkClick as EventListener,
+      );
+    };
+  }, [items]);
+
+  const handleNext = () => {
+    if (currentIndex < items.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleDragStart = (
+    event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>,
+  ) => {
+    const startX =
+      "touches" in event ? event.touches[0].clientX : event.clientX;
+    dragData.current = { startX, currentX: startX };
+  };
+
+  const handleDragMove = (
+    event: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>,
+  ) => {
+    if (!dragData.current) return;
+    const currentX =
+      "touches" in event ? event.touches[0].clientX : event.clientX;
+    const { startX } = dragData.current;
+    const diffX = currentX - startX;
+    const threshold = containerRef.current!.offsetWidth / 4;
+
+    if (diffX > threshold) {
+      handlePrev();
+      dragData.current = null;
+    } else if (diffX < -threshold) {
+      handleNext();
+      dragData.current = null;
+    } else {
+      dragData.current.currentX = currentX;
+    }
+  };
+
+  const handleDragEnd = () => {
+    dragData.current = null;
+  };
+
+  const swipeToIndex = (index: number) => {
+    if (index >= 0 && index < items.length) {
+      setCurrentIndex(index);
+    }
+  };
+
+  return (
+    <div
+      className="swiper-container"
+      ref={containerRef}
+      onMouseDown={handleDragStart}
+      onMouseMove={handleDragMove}
+      onMouseUp={handleDragEnd}
+      onTouchStart={handleDragStart}
+      onTouchMove={handleDragMove}
+      onTouchEnd={handleDragEnd}
+    >
+      <div
+        className={`swiper-navigation ${currentIndex > 0 ? "show-prev" : ""} ${currentIndex < items.length - 1 ? "show-next" : ""}`}
+      >
+        <button className="swiper-button-prev" onClick={handlePrev}>
+          &lt;
+        </button>
+        <button className="swiper-button-next" onClick={handleNext}>
+          &gt;
+        </button>
+      </div>
+      <div
+        className="swiper-wrapper"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={`swiper-slide ${index === currentIndex ? "active" : ""}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="pagination-container">
+        <div className="pagination-dots">
+          {items.map((_, index) => (
+            <div
+              key={index}
+              className={`pagination-dot ${index === currentIndex ? "active" : ""}`}
+              onClick={() => {
+                setCurrentIndex(index);
+              }}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="book-now-button">
+        <a
+          href="https://system.easypractice.net/book/proaktiv-idrottscenter"
+          target="_blank"
+          className="pointer-events-none"
+        >
+          <button className=" text-white font-bold py-2 px-4 rounded-lg border-white border-2 opacity-20">
+            Boka tid
+          </button>
+        </a>
+      </div>
+      <p className="contact-info text-xs tracking-widest">
+        Lidingövägen 1, 114 33 Stockholm
+      </p>
+    </div>
+  );
 };
 
 export default ServiceItemSwiper;
